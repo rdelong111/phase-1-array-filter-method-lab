b504f9ab775f2b68709a40cbc89a2a743759cf58
RyanDeLong <rdelong111@gmail.com>
2021-12-21 22:19:38 -0600

    commit tree 0eea6ca1b42c79a69049da785679374e29432dcf

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(x, s) {
+	return x.filter(matS => matS.toUpperCase() === s.toUpperCase());
+}
+
+function fuzzyMatch(x, s) {
+	return x.filter(matS => matS.indexOf(s) === 0);
+}
+
+function matchName(x, s) {
+	return x.filter(nameprop => nameprop.name === s);
+}
